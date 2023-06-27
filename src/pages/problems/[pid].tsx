4307582fa8301fe0d291221b323e5fc3a923956d
Xing Ern <lxingern@gmail.com>
2023-06-27 16:55:59 +0800

    use SSG to generate problem pages

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,16 +1,49 @@
 import Topbar from '@/components/Topbar/Topbar';
 import Workspace from '@/components/Workspace/Workspace';
 import React from 'react';
+import { problems } from '@/utils/problems'
+import { Problem } from '@/utils/types/problem';
 
 type ProblemPageProps = {
-  
+  problem: Problem
 };
 
-const ProblemPage:React.FC<ProblemPageProps> = () => {
-  
+const ProblemPage:React.FC<ProblemPageProps> = ({ problem }) => {
   return <div>
     <Topbar problemPage />
-    <Workspace />
+    <Workspace problem={problem} />
   </div>
 }
 export default ProblemPage;
+
+export async function getStaticPaths() {
+  const paths = Object.keys(problems).map(key => ({
+    params: {
+      pid: key
+    }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }:{ params: { pid:string } }) {
+  const { pid } = params
+  const problem = problems[pid]
+
+  if (!problem) {
+    return {
+      notFound: true
+    }
+  }
+
+  problem.handlerFunction = problem.handlerFunction.toString()
+
+  return {
+    props: {
+      problem
+    }
+  }
+}
